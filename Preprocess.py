293528f59995d2ff4dbc1893421bb498963e78ad
JyothsnaKat <jyothsnakat@gmail.com>
2023-10-26 22:45:52 +0200

    Preprocessed Data Stage 1

--- Preprocess.py
+++ Preprocess.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def process_csv(file_path):
+def process_csv(file_path, updatefilepath):
     # Read the csv file
     df = pd.read_csv(file_path)
 
@@ -13,10 +13,14 @@ def process_csv(file_path):
     # Calculate Total_power as the product of power_voltage_in_volts and power_current_in_amps
     df['Total_power'] = df['power_voltage_in_volts'] * df['power_current_in_amps']
 
+    # Round the 'Total_power' values 
+    df['TP_round'] = df['Total_power'].round(decimals = 2)
+
     # Save the modified dataframe back to the csv file
-    df.to_csv(file_path, index=False)
+    df.to_csv(updatefilepath, index=False)
 
 if __name__ == '__main__':
     file_path = input("Enter the path of the csv file: ")
-    process_csv(file_path)
+    updatefilepath = input("Enter new path to save processed file:")
+    process_csv(file_path, updatefilepath)
     print("File processed successfully!")
