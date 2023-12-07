de9548934b00b06df6e01c4dc3d58af63f98b2c4
Sachin <53938517+Sachin498@users.noreply.github.com>
2023-12-07 11:40:59 +0100

    Update Cliffs_delta.R

--- Cliffs_delta.R
+++ Cliffs_delta.R
@@ -1,23 +1,23 @@
 library(dplyr)
 library(effsize)
 
-data <- read.csv("D:/Green lab/EnergyConsumed.csv")
+data <- read.csv("D:/Green lab/final.csv")
 print(data)
 
 # Transform the energy consumed into domain specific data
-domain1_data <- filter(data, Domain_name == "Education")$Energy_consumed
-domain2_data <- filter(data, Domain_name == "Entertainment")$Energy_consumed
-domain3_data <- filter(data, Domain_name == "Gaming")$Energy_consumed
+Education <- filter(data, Domain_name == "Education")$Energy_consumed
+Entertainment <- filter(data, Domain_name == "Entertainment")$Energy_consumed
+Gaming <- filter(data, Domain_name == "Gaming")$Energy_consumed
 
 
 # To see difference between Education and Entertainment
-result <- cliff.delta(domain1_data, domain2_data)
+result <- cliff.delta(Education, Entertainment)
 print(result)
 
 # To see difference between Entertainment and Gaming
-result1 <- cliff.delta(domain2_data, domain3_data)
+result1 <- cliff.delta(Entertainment, Gaming)
 print(result1)
 
 # To see difference between Education and Gaming
-result2 <- cliff.delta(domain1_data, domain3_data)
+result2 <- cliff.delta(Education, Gaming)
 print(result2)
